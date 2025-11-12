c48143a7379f0e34c2a3e54020587c538ad7c898
priyanshu5179 <priyanshuyadav876585@gmail.com>
2025-11-12 23:45:13 +0530

    find greatestnu

---
+++ function/greatestnum.py
@@ -0,0 +1,8 @@
+def greatest(a,b,c):
+    if (a>b and a>c ):
+        print("a is greater")
+    elif(b>a and b>c):
+        print("b is greater")
+    else:
+        print("c is greater")
+greatest(3,5,7)        
